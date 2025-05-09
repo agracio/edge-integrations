d6b772afc96aaa8b4658ef23990a4a9cfef82b35
agracio <agracio@users.noreply.github.com>
2025-05-09 19:49:46 +0100

    updating CI

--- edge-sql.mssql.test.js
+++ edge-sql.mssql.test.js
@@ -1,7 +1,7 @@
+process.env.EDGE_USE_CORECLR = 1;
+const { expect } = require('chai');
 const edge = require('edge-js');
-const expect = require('@jest/globals').expect;
-const test = require('@jest/globals').test;
-const describe = require('@jest/globals').describe;
+
 
 function sql(func, params){
     return new Promise((resolve, reject) =>{
@@ -20,13 +20,10 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-describe("edge-sql MS-SQL tests", () => {
+describe("edge-sql MS-SQL", () => {
 
-    // test('no connection string', async() => {
-    //     expect(() =>edge.func('sql', function () {/**/})).toThrow(/^connectionString must be specified/);
-    // });
 
-    test('select top', async() => {
+    it('select top', async() => {
 
         let func = edge.func('sql', function () {/*
             select top 2 * from Authors
@@ -34,10 +31,10 @@ describe("edge-sql MS-SQL tests", () => {
 
         let result = await sql(func, null);
 
-        expect(JSON.stringify(result) ).toBe('[{\"Id\":1,\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"},{\"Id\":2,\"Name\":\"Author - 2\",\"Country\":\"Country - 2\"}]');
+        expect(JSON.stringify(result) ).to.equal('[{\"Id\":1,\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"},{\"Id\":2,\"Name\":\"Author - 2\",\"Country\":\"Country - 2\"}]');
     });
 
-    test('select id', async() => {
+    it('select id', async() => {
 
         let func = edge.func('sql', function () {/*
             select * from Authors 
@@ -46,10 +43,10 @@ describe("edge-sql MS-SQL tests", () => {
 
         let result = await sql(func, { authorId: 1 });
 
-        expect(JSON.stringify(result) ).toBe('[{\"Id\":1,\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"}]');
+        expect(JSON.stringify(result) ).to.equal('[{\"Id\":1,\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"}]');
     });
 
-    test('select id with options', async() => {
+    it('select id with options', async() => {
 
         let func = edge.func('sql', {
             source: 'select * from Authors where Id = @authorId',
@@ -58,10 +55,10 @@ describe("edge-sql MS-SQL tests", () => {
 
         let result = await sql(func, { authorId: 1 });
 
-        expect(JSON.stringify(result) ).toBe('[{\"Id\":1,\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"}]');
+        expect(JSON.stringify(result) ).to.equal('[{\"Id\":1,\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"}]');
     });
 
-    test('select from multiple tables', async() => {
+    it('select from multiple tables', async() => {
 
         let func = edge.func('sql', {
             source: 'select top 1 * from Authors; select top 1 * from Books',
@@ -70,14 +67,14 @@ describe("edge-sql MS-SQL tests", () => {
 
         let result = await sql(func, null);
 
-        expect(JSON.stringify(result) ).toBe('{\"Authors\":[{\"Id\":1,\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"}],\"Books\":[{\"Id\":1,\"Author_id\":485,\"Price\":64,\"Edition\":9}]}');
+        expect(JSON.stringify(result) ).to.equal('{\"Authors\":[{\"Id\":1,\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"}],\"Books\":[{\"Id\":1,\"Author_id\":485,\"Price\":64,\"Edition\":9}]}');
     });
 
-    test('update', async() => {
+    it('update', async() => {
 
         const rnd = random(3, 500);
         let name = 'Author - ' + rnd;
-        console.log(rnd)
+        // console.log(rnd)
 
         let func = edge.func('sql', {
             source: 'select Name from Authors where Id = @authorId',
@@ -85,7 +82,7 @@ describe("edge-sql MS-SQL tests", () => {
         });
 
         let result = await sql(func, { authorId: rnd});
-        expect(result[0].Name).toBe(name);
+        expect(result[0].Name).to.equal(name);
 
         let update = edge.func('sql', {
             source: 'update Authors set Name = @name where Id = @authorId',
@@ -97,15 +94,15 @@ describe("edge-sql MS-SQL tests", () => {
         await sql(update, { authorId: rnd, name: newName});
 
         result = await sql(func, { authorId: rnd});
-        expect(result[0].Name).toBe(newName);
+        expect(result[0].Name).to.equal(newName);
 
         await sql(update, { authorId: rnd, name: name});
 
         result = await sql(func, { authorId: rnd});
-        expect(result[0].Name).toBe(name);
+        expect(result[0].Name).to.equal(name);
     });
 
-    test('stored proc', async() => {
+    it('stored proc', async() => {
 
         let func = edge.func('sql', {
             source: 'exec GetBooksByAuthor',
@@ -114,10 +111,10 @@ describe("edge-sql MS-SQL tests", () => {
 
         let result = await sql(func, { AuthorId: 2});
 
-        expect(JSON.stringify(result) ).toBe('[{\"Id\":182,\"Author_id\":2,\"Price\":55,\"Edition\":10},{\"Id\":279,\"Author_id\":2,\"Price\":88,\"Edition\":9}]');
+        expect(JSON.stringify(result) ).to.equal('[{\"Id\":182,\"Author_id\":2,\"Price\":55,\"Edition\":10},{\"Id\":279,\"Author_id\":2,\"Price\":88,\"Edition\":9}]');
     });
 
-    test('stored proc with output parameters', async() => {
+    it('stored proc with output parameters', async() => {
 
         let func = edge.func('sql', {
             source: 'exec GetAuthorDetails',
@@ -126,6 +123,6 @@ describe("edge-sql MS-SQL tests", () => {
 
         let result = await sql(func, { AuthorID: 1, '@returnParam1': 'Name', '@returnParam2': 'Country' });
 
-        expect(JSON.stringify(result) ).toBe('{\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"}');
+        expect(JSON.stringify(result) ).to.equal('{\"Name\":\"Author - 1\",\"Country\":\"Country - 1\"}');
     });
 });
